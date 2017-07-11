17420684a3fb87a3761f31a32b916fb2cb5f18d8
wangpei <p.wang@avid.ly>
2017-07-11 20:09:36 +0800

    更新登录功能

--- handlers/login.py
+++ handlers/login.py
@@ -13,24 +13,63 @@ class LoginHandler(BaseHandler):
     self.render("login.html")
 
   def post(self):
-    email = self.get_body_argument("email")
-    password = self.get_body_argument("password")
-    password = base64.b64encode(password)
-    cbox_remember = self.get_body_argument("cbox_remember",default="on")
-
-    if not self._checkemail_action(email):
-      self.redirect("/sign_up?error=not_exists&user={0}".format(email))
-    else:
-      if not self._checkpasswd_action(email,password):
-        self.redirect("/login?error=passwd_error")
+    email = ''
+    username = ''
+    account = self.get_body_argument('account')
+    if '@' in account:
+      email = account
+      password = self.get_body_argument("password")
+      password = base64.b64encode(password)
+      cbox_remember = self.get_body_argument("cbox_remember",default="on")
+      if not self._checkemail_action(email):
+        self.redirect("/sign_up?error=not_exists&user={0}".format(email))
       else:
-        ret = self.db.query("SELECT username FROM user WHERE email=%s",email)
-        username = ret[0]['username']
-        if cbox_remember == "on":
-          self.set_secure_cookie("username",username,expires_days=30)
+        if not self._checkpasswd_action(email,password):
+          self.redirect("/login?error=passwd_error")
         else:
-          self.set_secure_cookie("username",username,expires_days=1)
-        self.redirect('/')
+          ret = self.db.query("SELECT username FROM user WHERE email=%s",email)
+          username = ret[0]['username']
+          if cbox_remember == "on":
+            self.set_secure_cookie("username",username,expires_days=30)
+          else:
+            self.set_secure_cookie("username",username,expires_days=1)
+          self.redirect('/')
+    else:
+      username = account
+      password = self.get_body_argument("password")
+      password = base64.b64encode(password)
+      cbox_remember = self.get_body_argument("cbox_remember",default="on")
+      if not self._checkname_action(username):
+        self.redirect("/sign_up?error=not_exists&user={0}".format(username))
+      else:
+        if not self._checkpasswd_action(username,password):
+          self.redirect("/login?error=passwd_error")
+        else:
+          if cbox_remember == "on":
+            self.set_secure_cookie("username",username,expires_days=30)
+          else:
+            self.set_secure_cookie("username",username,expires_days=1)
+          self.redirect('/')
+
+    ##password = self.get_body_argument("password")
+    #password = base64.b64encode(password)
+    #cbox_remember = self.get_body_argument("cbox_remember",default="on")
+
+    #if not self._checkemail_action(email):
+    #  self.redirect("/sign_up?error=not_exists&user={0}".format(email))
+    #elif not self._checkname_action(username):
+    #  self.redirect("/sign_up?error=not_exists&user={0}".format(username))
+    #else:
+    #  if not self._checkpasswd_action(email,password):
+    #    self.redirect("/login?error=passwd_error")
+    #  else:
+    #    ret = self.db.query("SELECT username FROM user WHERE email=%s",email)
+    #    username = ret[0]['username']
+    #    if cbox_remember == "on":
+    #      self.set_secure_cookie("username",username,expires_days=30)
+    #    else:
+    #      self.set_secure_cookie("username",username,expires_days=1)
+    #    self.redirect('/')
 
   def _checkemail_action(self,email):
     id = self.db.query("SELECT id FROM user WHERE email=%s",email)
@@ -39,8 +78,18 @@ class LoginHandler(BaseHandler):
     else:
       return True
 
-  def _checkpasswd_action(self,email,password):
-    id = self.db.query("SELECT id FROM user WHERE (email=%s and password=%s)",email,password)
+  def _checkname_action(self,username):
+    id = self.db.query("SELECT id FROM user WHERE username=%s",username)
+    if len(id) == 0:
+      return False
+    else:
+      return True
+
+  def _checkpasswd_action(self,account,password):
+    if '@' in account:
+      id = self.db.query("SELECT id FROM user WHERE (email=%s and password=%s)",account,password)
+    else:
+      id = self.db.query("SELECT id FROM user WHERE (username=%s and password=%s)",account,password)
     if not id:
       return False
     else:
